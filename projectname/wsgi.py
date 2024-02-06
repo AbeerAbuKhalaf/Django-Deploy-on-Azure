cf92f827ccf45cfa6979b7fc9479e32e72aa1edc
AbeerAbuKhalaf <137313824+AbeerAbuKhalaf@users.noreply.github.com>
2024-02-07 00:35:58 +0300

    Update wsgi.py

--- projectname/wsgi.py
+++ projectname/wsgi.py
@@ -1,10 +1,9 @@
 import os
-from django.core.wsgi import get_wsgi_application
 
 # Check for the PRODUCTION environment variable to see if we are running in Azure App Service
 # If so, then load the settings from production.py
 settings_module = 'projectname.production' if 'PRODUCTION' in os.environ else 'projectname.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
